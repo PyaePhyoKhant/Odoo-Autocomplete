5b2065e0f3e117ede32f28e9ceb7c379cdcf3773
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2019-12-02 22:25:28 +0630

    add some simple xml tags, keys, values

--- src/extension.ts
+++ src/extension.ts
@@ -20,11 +20,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
+			// tags
 			const attribute = new vscode.CompletionItem('attribute');
+			const record = new vscode.CompletionItem('record');
+			const field = new vscode.CompletionItem('field');
+			const button = new vscode.CompletionItem('button');
+			const xpath = new vscode.CompletionItem('xpath');
+			
+			// keys
+			const id = new vscode.CompletionItem('id');
+			const model = new vscode.CompletionItem('model');
+			const name = new vscode.CompletionItem('name');
+			const positionn = new vscode.CompletionItem('position');
+
+			// values
+			const attributes = new vscode.CompletionItem('attributes');
+			const before = new vscode.CompletionItem('before');
+			const after = new vscode.CompletionItem('after');
 
 			// return all completion items as array
 			return [
-				attribute
+				attribute, record, field, button, xpath,
+				id, model, name, positionn,
+				attributes, before, after
 			];
 		}
 	});
